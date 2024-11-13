277ed0fe9e340bff258a4e28a9b419fa38ef3ab2
Derick Willer <derickwiller09@gmail.com>
2024-11-12 22:41:35 -0300

    properties, Game, ORM

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,8 +1,18 @@
 package com.devsuperior.dslist.entities;
+import jakarta.persistence.*;
 
+import java.util.Objects;
+@Entity
+@Table(name = "tb_game")
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    
+    @Column(name = "game_year")
+    private Integer year;
     private String genre;
     private String platform;
     private String imgUrl;
@@ -13,14 +23,91 @@ public class Game {
         
     }
 
-    public Game(Long id, String title, String genre, String platform, String imgUrl, String longDescription, String shortDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
+        this.year = year;
         this.genre = genre;
         this.platform = platform;
         this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
         this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
-    
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
